a2026bafc97254f0d6abc0cc1efc91b618aa7f76
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-02-24 17:36:20 +0900

    Added open-store and comments in tests/run.lisp

--- tests/run.lisp
+++ tests/run.lisp
@@ -1,9 +1,16 @@
 ; run tests
+; 1. replace the parameter of open-store
+;    http://common-lisp.net/project/elephant/doc/elephant.html#Getting-Started
+; 2. compile and load this file
+; 3. run any test you want
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (asdf:oos 'asdf:load-op :web4r-tests))
 
 (in-package :web4r-tests)
+
+(ele:open-store
+ '(:clsql (:postgresql "localhost" "test" "postgres" "pgpass")))
 
 ; run each test
 ;(5am:run! 'server)
