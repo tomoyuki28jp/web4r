e99afd2c3d0a9d6ade5d2db079fd30362dc6f31d
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-04-17 07:40:12 +0900

    Added tests for defpage (page-lambda)

--- tests/server.lisp
+++ tests/server.lisp
@@ -17,7 +17,20 @@
   (defpage test/test (path1 path2)
     (p "path1: " path1 " path2: " path2))
   (is (string= (http-request (page-uri "test" "test" "foo" "bar"))
-                  "path1: foo path2: bar")))
+                  "path1: foo path2: bar"))
+  (defpage test (:get g1 g2) (p g1 " " g2))
+  (is (string= (http-request (add-parameters (page-uri "test") "g1" "v1" "g2" "v2"))
+               "v1 v2"))
+  (defpage test (:post p1 p2) (p p1 " " p2))
+  (is (string= (http-request (page-uri "test") :method :post
+                             :parameters '(("p1" . "v1") ("p2" . "v2")))
+               "v1 v2"))
+  (defpage test/test (path1 path2 :post p1 p2 :get g1 g2)
+    (p (join " " path1 path2 p1 p2 g1 g2)))
+  (is (string= (http-request (add-parameters (page-uri "test" "test" "pp1" "pp2")
+                                             "g1" "gv1" "g2" "gv2")
+                             :method :post :parameters '(("p1" . "pv1") ("p2" . "pv2")))
+               "pp1 pp2 pv1 pv2 gv1 gv2")))
 
 (test set/get-page
   (setf web4r::*pages* nil)
