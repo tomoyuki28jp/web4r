8813419109dbdc0cbcb8fbb085b6c1bd46fa1ad4
Tomo Matsumoto <tomo@X300.(none)>
2009-05-15 20:58:06 +0900

    Wrote documentation string for specials variables in src/specials.lisp

--- src/specials.lisp
+++ src/specials.lisp
@@ -3,77 +3,89 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defvar *web4r-dir*
     (asdf:component-pathname (asdf:find-system 'web4r))
-    "Pathname of the web4r directory")
+    "A pathname of the web4r directory")
 
   (defvar *public-dir* (merge-pathnames "public/" *web4r-dir*)
-    "Pathname of the public directory")
+    "A pathname of the public directory")
 
   (defvar *sml-dir*    (merge-pathnames "sml/"    *web4r-dir*)
-    "Pathname of the sml directory")
+    "A pathname of the sml directory")
 
   (defvar *example-bdb*
     `(:BDB ,(merge-pathnames "examples/bdb/" *web4r-dir*))
-    "BDB directory for example applications")
+    "A pathname of the BDB database directory for example applications")
 
   (defvar *web4r-package* (find-package :web4r)
-    "the web4r package designator"))
+    "The web4r package designator"))
 
-(defvar *upload-save-dir* "/tmp/web4r/upload/"
-  "Directory path to save upload files")
+(defvar *upload-save-dir* #P"/tmp/web4r/upload/"
+  "A pathname of the directory to save uploaded files. 
+The default is #P\"/tmp/web4r/upload/\".")
 (ensure-directories-exist *upload-save-dir* :verbose nil)
 
-(defvar *tmp-save-dir* "/tmp/web4r/tmp/"
-  "Directory path to save temporary uploaded files")
+(defvar *tmp-save-dir* #P"/tmp/web4r/tmp/"
+  "A pathname of the directory to save temporary uploaded files. 
+The default is #P\"/tmp/web4r/tmp/\".")
 (ensure-directories-exist *tmp-save-dir*    :verbose nil)
 
 (defvar *tmp-files-gc-lifetime* 1440
-  "Temporary saved files lifetime")
+  "The lifetime of temporary saved files in number of seconds.
+The default is 1440 (24 minutes).")
 
 (defvar *tmp-files-gc-probability* 100
-  "Probability to start a gc process for temporary saved files")
+  "The probability to start a gc process for temporary saved files. 
+The default is 100.")
 
 (defvar *image-public-dirs*
   `(("upload" . *upload-save-dir*)
     ("tmp"    . *tmp-save-dir*))
-  "Alist of image public directories")
+  "An alist of image public directories: a string name -> a pathname of the 
+public directory")
 
 (defvar *debug-log-file* #P"/tmp/web4r/debug.log"
-  "Pathname of the debug log file")
+  "A pathname of the debug log file. The default is #P\"/tmp/web4r/debug.log\".")
 
 (defvar *debug-mode* nil
-  "Debug mode flag")
+  "If this is non-nil, debug-mode is turned on and off otherwise.")
 
 (defvar *sid->cid*  (make-hash-table)
-  "sid(session-id) -> cid(continuation-id) : mapping table")
+  "A mapping hash table: sid(session-id) -> cid(continuation-id)")
 
 (defvar *cid->cont* (make-hash-table :test 'equal)
-  "cid(continuation-id) -> instance of the cont structure")
+  "A mapping hash table: cid(continuation-id) -> an instance of the cont structure")
 
 (defvar *cid-generated-order*
   (make-array 0 :fill-pointer 0 :adjustable t)
-  "cids by the order of their generated time.
-   this is used to destroy expired continuations")
+  "cids(continuation-ids) by the order of their generated time. 
+This is used to destroy expired continuations.")
 
 (defvar *cont-gc-lifetime* 1440
-  "continuation lifetime")
+  "The lifetime of continuations in number of seconds. 
+The default is 1440 (24 minutes).")
 
 (defvar *cont-gc-probability* 100
-  "probability to start a gc process")
+  "The probability to start a gc process for expired continuations. 
+The default is 100.")
 
 (defvar *cont-sessions* (make-hash-table :test 'equal)
-  "continuations based session data")
+  "A hash table of continuation based session data: a string cid(continuation-id) 
+-> alist of key and value pairs.")
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defvar *last-posts* nil))
+  (defvar *last-posts* nil
+    "This is only used on the process to expand last-post inside a continuation"))
 
 (defvar *page-uri-paths* 0
-  "The number of current page uri paths")
+  "The number of the current page uri paths. 
+Example: (defpage one/two/three () web4r::*page-uri-paths*) ;=> 3")
 
 (defvar *pages* nil
-  "Page paths -> handler alist")
+  "An alist of page handlers: nested page uri paths -> a handler function. 
+Example: (defpage onw/two/three () ...) => (\"onw\" (\"two\" (\"three\" (NIL . #))")
 
 (defvar *msgs* nil
-  "Instance of the msgs structure")
+  "This is an instance of msgs/error-msgs if there are messages to display 
+and nil otherwise")
 
 (defvar *error-formats*
   '((:invalid      . "~A is invalid")
@@ -86,9 +98,11 @@
     (:not-alpha    . "~A must contains only alphabetic characters")
     (:not-alnum    . "~A must contains only alphabetic and digit characters")
     (:not-a-unique . "The same ~A has already been registered")
-    (:not-a-image  . "~A must be a jpeg, png or gif image file.")))
+    (:not-a-image  . "~A must be a jpeg, png or gif image file."))
+  "An alist of validation error messages: a keyword key -> a string error message")
 
-(defvar *validators* (make-hash-table))
+(defvar *validators* (make-hash-table)
+  "A hash table: a keyword name of validator -> a validator function")
 
 (defvar *valid-email-format*
   (remove #\Newline
@@ -96,32 +110,49 @@
 (?:\.(?:[a-zA-Z0-9_!#\$\%&'*+/=?\^`{}~|\-]+))*)|
 (?:\"(?:\\[^\r\n]|[^\\\"])*\")))\@
 (?:(?:(?:[a-zA-Z0-9_!#\$\%&'*+/=?\^`{}~|\-]+)
-(?:\.(?:[a-zA-Z0-9_!#\$\%&'*+/=?\^`{}~|\-]+))*))$"))
+(?:\.(?:[a-zA-Z0-9_!#\$\%&'*+/=?\^`{}~|\-]+))*))$")
+  "The regular expression used to validate a email address")
 
 (defvar *page-param* "page"
-  "Name of a page get parameter")
+  "The name of get parameter denotate the current page number. 
+The default is 'page'.")
 
-(defvar *items-per-page* 10)
-(defvar *links-per-page* 10)
+(defvar *items-per-page* 10
+  "The default number to display items per page. The default is 10.")
+
+(defvar *links-per-page* 10
+  "The default number to display links per page. The default is 10.")
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defvar *slots* (make-hash-table)))
+  (defvar *slots* (make-hash-table)
+    "A hash table: a symbol name of a pclass => a list of slot-options instances"))
 
-(defvar *with-slots* nil)
+(defvar *with-slots* nil
+  "If this is :all, all pclass slots are showed. Otherwise a list of pclass 
+slots to show for a page.")
 
-(defvar *without-slots* nil)
+(defvar *without-slots* nil
+  "A list of pclass slots to hide for a page.")
 
 (defvar *max-items-per-page* 50
-  "Maximum number to display items per page")
+  "The maximum number to display items per page. This value is only used when a 
+user changes a value of the get parameter named items_per_page and the value 
+exceeeds this maximum number. The default is 50.")
 
 (defvar *max-links-per-page* 30
-  "Maximum number to display links per page")
+  "The maximum number to display links per page. This value is only used when a 
+user changes a value of the get parameter named links_per_page and the value 
+exceeeds this maximum number. The default is 30.")
 
 (defvar *login-msgs*
   '((:login-failed      . "Wrong username and password combination")
     (:login-succeeded   . "Logged in")
     (:logged-out        . "Logged out")
-    (:already-logged-in . "You are already logged in")))
+    (:already-logged-in . "You are already logged in"))
+  "An alist of log in/out messages: a keyword key -> a string message")
 
-(defvar *thumbnail-width*  100)
-(defvar *thumbnail-height* 100)
+(defvar *thumbnail-width*  100
+  "The default thumbnail width. The default is 100.")
+
+(defvar *thumbnail-height* 100
+  "The default thumbnail height. The default is 100.")
