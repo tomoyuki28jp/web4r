13fdbff042d9a9e780873544d0666e4c8cde8530
Tomo Matsumoto <tomo@X300.(none)>
2009-05-16 04:45:39 +0900

    Wrote documentation string for functions/macros in src/debug.lisp

--- src/debug.lisp
+++ src/debug.lisp
@@ -3,15 +3,16 @@
 ; --- Util ------------------------------------------------------
 
 (defun ps (instance)
- "print each slot's name and value"
+  "Prints each slot's name and value of the INSTANCE for debugging"
   (loop for slot in (ele::class-slots (class-of instance))
         as name = (ele::slot-definition-name slot)
-        do (print  (list name
-                         (or (ignore-errors
-                               (slot-value instance name))
-                             :unbound)))))
+        do (print (list name
+                        (or (ignore-errors
+                              (slot-value instance name))
+                            :unbound)))))
 
 (defmacro with-post-parameters (parameters &rest body)
+  "Executes the BODY with the alist of post parameters PARAMETERS"
   `(let* ((*acceptor* (make-instance 'acceptor))
           (*reply*    (make-instance (acceptor-reply-class *acceptor*)))
           (*request*  (make-instance 'request :headers-in '())))
@@ -19,6 +20,7 @@
      ,@body))
 
 (defmacro with-get-parameters (parameters &rest body)
+  "Executes the BODY with the alist of get parameters PARAMETERS"
   `(let* ((*acceptor* (make-instance 'acceptor))
           (*reply*    (make-instance (acceptor-reply-class *acceptor*)))
           (*request*  (make-instance 'request :headers-in '())))
@@ -27,9 +29,10 @@
 
 ; --- Logging ---------------------------------------------------
 
-(defun debug-log (&rest args)
+(defun debug-log (&rest contents)
+  "Write a log CONTENTS to the *debug-log-file*"
   (let ((*message-log-pathname* *debug-log-file*))
-    (log-message :debug (join " " args))))
+    (log-message :debug (join " " contents))))
 
 (defun backtrace-log (error)
   (when *debug-mode*
@@ -42,9 +45,11 @@
 ; --- Debug mode ------------------------------------------------
 
 (defun debug-mode-on ()
+  "Turns on debug-mode"
   (setf *debug-mode* t
         *show-lisp-errors-p* t))
 
 (defun debug-mode-off ()
+  "Turns off debug-mode"
   (setf *debug-mode* nil
         *show-lisp-errors-p* nil))
