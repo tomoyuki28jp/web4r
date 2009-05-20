21eff0c31fd22297001886c2db2ae6fc72295d6a
Tomo Matsumoto <tomo@X300.(none)>
2009-05-21 03:25:38 +0900

    Wrote documentation strings for src/server.lisp

--- src/server.lisp
+++ src/server.lisp
@@ -6,37 +6,60 @@
 ; --- Util ------------------------------------------------------
 
 (defun uri-path (n &optional (request *request*))
+  "Returns the number of the current page uri paths. For example, when 
+the current page is (defpage one/two/three () ...), this returns 3."
   (nth (1- (+ n *page-uri-paths*))
        (split #\/ (car (split #\? (request-uri* request))))))
 
 (defun host-uri ()
+  "Returns the 'X-FORWARDED-HOST' or 'Host' incoming http header value."
   (let ((host (or (header-in "X-FORWARDED-HOST" *request*)
                   (host))))
     (concat "http://" host "/")))
 
-(defun page-uri (&rest args)
-  (concat (host-uri) (apply #'join "/" args) "/"))
+(defun page-uri (&rest paths)
+  "Generates a page uri from PATHS and returns it. 
+Example: (page-uri 'page' 'path1' 'path1') 
+;=> 'http://localhost:8080/page/path1/path1/'"
+  (concat (host-uri) (apply #'join "/" paths) "/"))
 
 (defun file-data (name n)
+  "Returns (nth N (post-parameter NAME))."
   (aand (post-parameter name) (listp it) (nth n it)))
 
-(defun file-path (name) (file-data name 0))
-(defun file-name (name) (file-data name 1))
-(defun file-type (name) (file-data name 2))
+(defun file-path (name)
+  "If the value of the post parameter named by the NAME is a file, 
+returns a pathname denoting the place where the uploaded file was stored."
+  (file-data name 0))
 
-(defun set-post-parameters (params)
-  (setf (slot-value *request* 'hunchentoot::post-parameters) params))
+(defun file-name (name)
+  "If the value of the post parameter named by the NAME is a file, 
+returns the file name sent by the browser."
+  (file-data name 1))
 
-(defun set-get-parameters (params)
-  (setf (slot-value *request* 'hunchentoot::get-parameters) params))
+(defun file-type (name)
+  "If the value of the post parameter named by the NAME is a file, 
+returns the content type sent by the browser."
+  (file-data name 2))
+
+(defun set-post-parameters (parameters)
+  "Sets the post PARAMETERS. PARAMETERS must be an alist."
+  (setf (slot-value *request* 'hunchentoot::post-parameters) parameters))
+
+(defun set-get-parameters (parameters)
+  "Sets the get PARAMETERS. PARAMETERS must be an alist."
+  (setf (slot-value *request* 'hunchentoot::get-parameters)  parameters))
 
 (defun start-server (&optional acceptor)
+  "Starts ACCEPTOR so that it begins accepting connections. 
+Returns the acceptor."
   (start (or acceptor
              (make-instance 'acceptor :port 8080
                :request-dispatcher 'web4r-dispatcher))))
 
 (defun stop-server (acceptor)
-  (stop  acceptor))
+  "Stops ACCEPTOR so that it no longer accepts requests."
+  (stop acceptor))
 
 ; --- Public directories ----------------------------------------
 
@@ -81,6 +104,15 @@
     (setf *pages* (replace-assoc* idx *pages* fn :test #'equal))))
 
 (defmacro page-lambda ((&rest args) &body body)
+  "Creates a page procedure.
+Syntax:
+page-lambda ([path ...] [:get garg ...] [:post parg ...] :auth :redirect uri) body
+path---a uri path
+garg---a get parameter name
+parg---a post parameter name
+:auth---if :auth is passed and a user hasn't logged, redirect the user to the login page
+uri---the uri to redirect a user after logging in
+body---a form"
   (flet ((args (args) (loop for a in args until (keywordp a) collect a))
          (car*  (x) (if (listp x) (car x) x))
          (cadr* (x) (when (listp x) (cadr x))))
@@ -101,14 +133,26 @@
                (regist-page (or ,(cadr (member :redirect args)) (host-uri)))
                (progn ,@body)))))))
 
-(defmacro defpage (name (&rest args) &rest body)
+(defmacro defpage (page (&rest args) &rest body)
+  "Defines a new page named PAGE. Users can visit the page by accessing 
+an uri like 'http://yourhost/PAGE'.
+Syntax:
+defpage ([path ...] [:get garg ...] [:post parg ...] :auth :redirect uri :default) body
+path---a uri path
+garg---a get parameter name
+parg---a post parameter name
+:auth---if :auth is passed and a user hasn't logged, redirect the user to the login page
+uri---the uri to redirect a user after logging in
+:default---if :default is passed, the page procedure is set to *default-handler*
+body---a form"
   `(progn
-     (set-page (->string-down ',name) (page-lambda (,@args) ,@body))
+     (set-page (->string-down ',page) (page-lambda (,@args) ,@body))
      (when (member :default ',args)
        (setf *default-handler*
-             (lambda () (page (->string-down ',name)))))))
+             (lambda () (page (->string-down ',page)))))))
 
 (defun page (page &rest args)
+  "Calls the procedure for the PAGE with ARGS"
   (multiple-value-bind (fn paths) (get-page page)
     (aif fn
          (let ((*page-uri-paths* paths))
@@ -126,17 +170,28 @@
   (let ((msgs (make-instance type :msgs (->list msgs))))
     (add-parameter uri "mid" (set-cont msgs))))
 
-(defun redirect/msgs (uri msgs)       (redirect (w/mid uri msgs 'msgs)))
-(defun redirect/error-msgs (uri msgs) (redirect (w/mid uri msgs 'error-msgs)))
+(defun redirect/msgs (uri messages)
+  "Redirects to the URI with the success MESSAGES"
+  (redirect (w/mid uri messages 'msgs)))
+
+(defun redirect/error-msgs (uri messages)
+  "Redirects to the URI with the error MESSAGES"
+  (redirect (w/mid uri messages 'error-msgs)))
 
 (defmacro w/msgs (msgs page args type)
   `(let ((*msgs* (make-instance ,type :msgs (->list ,msgs))))
      (apply #'page ,page ,args)))
 
-(defun page/msgs (page msgs &rest args)       (w/msgs msgs page args 'msgs))
-(defun page/error-msgs (page msgs &rest args) (w/msgs msgs page args 'error-msgs))
+(defun page/msgs (page messages &rest args)
+  "Calls the procedure for the PAGE with success MESSAGES and ARGS"
+  (w/msgs messages page args 'msgs))
+
+(defun page/error-msgs (page messages &rest args)
+  "Calls the procedure for the PAGE with error MESSAGES and ARGS"
+  (w/msgs messages page args 'error-msgs))
 
 (defun get-msgs ()
+  "Returns a list of messages to display if any."
   (awhen (or (when-let (mid (mid))
                (awhen (get-cont mid) (destroy-cont mid) it))
              *msgs*)
@@ -144,6 +199,7 @@
       it)))
 
 (defun msgs ()
+  "Prints messages if any."
   (awhen (get-msgs)
     (when-let (msgs (slot-value it 'msgs))
       (case (type-of it)
