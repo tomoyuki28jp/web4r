e537caba7dfe457433c10cc56d21351e374718f3
Tomo Matsumoto <tomo@X300.(none)>
2009-05-21 03:22:16 +0900

    Wrote documentation strings for src/valid.lisp

--- src/valid.lisp
+++ src/valid.lisp
@@ -2,31 +2,38 @@
 
 ; --- Util ------------------------------------------------------
 
-(defun error-msg (type &rest args)
-  (awhen (cdr (assoc type *error-formats*))
+(defun error-msg (error &rest args)
+  "Returns a string validation error message for the ERROR."
+  (awhen (cdr (assoc error *error-formats*))
     (apply #'format (append (list nil it) args))))
 
 (defun leap-year-p (year)
+  "Returns true if the YEAR is a leap year and nil otherwise."
   (and (plusp year)
        (zerop (mod year 4))
        (or (plusp (mod year 100))
            (zerop (mod year 400)))))
 
 (defun days-of (year month)
+  "Returns the last day of the MONTH of the YEAR."
   (let ((feb (if (leap-year-p year) 29 28)))
     (nth (1- month) `(31 ,feb 31 30 31 30 31 31 30 31 30 31))))
 
-(defun valid-date-p (y m d)
-  (let ((y (->int y)) (m (->int m)) (d (->int d)))
-    (when (and y m d)
-      (aand (days-of y m) (<= d it) t))))
+(defun valid-date-p (year month day)
+  "Returns true if the date if valid and nil otherwise."
+  (let ((year (->int year)) (month (->int month)) (day (->int day)))
+    (when (and year month day)
+      (aand (days-of year month) (<= day it) t))))
 
 (defun empty (x)
+  "Returns true if X is nil, '' or a list contains only nil and nil otherwise."
   (let ((x (if (listp x) (remove nil x) x)))
     (when (or (null x) (equal x ""))
       t)))
 
 (defun unique-p (class slot value &optional ins)
+  "Returns nil if the same VALUE has been registered in the SLOT of the CLASS 
+and true otherwise."
   (let* ((i (get-instances-by-value class slot value))
          (l (length i)))
     (or (= l 0)
@@ -35,10 +42,12 @@
 ; --- Validators ------------------------------------------------
 
 (defmacro define-validator (name args &rest body)
+  "Defines a new validator named NAME."
   `(setf (gethash (->keyword ',name) *validators*)
          (lambda ,args ,@body)))
 
 (defun get-validator (name)
+  "Returns a validator named NAME."
   (gethash name *validators*))
 
 (define-validator length (label value args)
@@ -82,14 +91,29 @@
 
 ; --- Validations -----------------------------------------------
 
-(defun validation-errors (label value validations)
-  (loop for (type args) on validations by #'cddr
+
+(defun validation-errors (label value validators)
+  "Validates the VALUE with VALIDATORS and returns error messages if any. 
+LABEL is used as the subject of the error messages.
+Example:
+ (validation-errors \"label\" \"12345\" '(:length 3))
+ ;=> (\"label is too long (maximum is 3 characters)\")"
+  (loop for (type args) on validators by #'cddr
         as validator = (or (get-validator type)
                            (error "invalid validator: ~A" type))
         as error = (funcall validator label value args)
         when error collect error until error))
 
 (defmacro with-validations (validations error-handler body)
+  "Executes the VALIDATIONS and the ERROR-HANDLER if any error 
+and BODY otherwise. The ERROR-HANDLER takes one argument which is 
+a list of validation error messages.
+Example:
+ (with-validations ((\"1\" \"v\" '(:required t))
+                    (\"2\" nil '(:required t)))
+   (lambda (e) e)
+   \"ok\")
+ ; => (\"2 can't be empty\")"
   `(aif (append ,@(loop for v in validations
                         collect `(validation-errors ,@v)))
         (funcall ,error-handler it)
