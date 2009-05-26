14dbc005497c81dca7a6a6ef7a55b9876c219636
Tomo Matsumoto <tomo@X300.(none)>
2009-05-26 16:22:08 +0900

    Updated docstrings in src/debug.lisp

--- src/debug.lisp
+++ src/debug.lisp
@@ -13,7 +13,7 @@
 
 (defmacro with-post-parameters (parameters &rest body)
   "Executes BODY with post PARAMETERS. PARAMETERS must be an alist
- of a key/value pairs."
+ of key/value pairs."
   `(let* ((*acceptor* (make-instance 'acceptor))
           (*reply*    (make-instance (acceptor-reply-class *acceptor*)))
           (*request*  (make-instance 'request :headers-in '())))
@@ -22,7 +22,7 @@
 
 (defmacro with-get-parameters (parameters &rest body)
   "Executes BODY with get PARAMETERS. PARAMETERS must be an alist
- of a key/value pairs."
+ of key/value pairs."
   `(let* ((*acceptor* (make-instance 'acceptor))
           (*reply*    (make-instance (acceptor-reply-class *acceptor*)))
           (*request*  (make-instance 'request :headers-in '())))
