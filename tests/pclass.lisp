392d74a288d52c61d6f4ebaeec88b89487b40978
Tomo Matsumoto <tomo@X300.(none)>
2009-06-07 21:43:43 +0900

    Added tests for select-date

--- tests/pclass.lisp
+++ tests/pclass.lisp
@@ -403,6 +403,57 @@ world"         (slot-save-value (get-slot 'testdb1 'note))))))
   (is (string=* "<label for=\"testdb1_image\">Image</label>"
                 (sml->ml (form-label (get-slot 'testdb1 'image))))))
 
+(test select-date
+  (is (string=* (sml->ml (select-date
+                          "name" :y 2008 :m 09 :d 28 :y-start 2007 :y-end 2009))
+"<span class=\"change_date\"><select name=\"name_y\" id=\"name_y\" class=\"y\"><option value=\"2007\">2007</option>
+<option value=\"2008\" selected=\"selected\">2008</option>
+<option value=\"2009\">2009</option>
+</select><select name=\"name_m\" id=\"name_m\" class=\"m\"><option value=\"1\">1</option>
+<option value=\"2\">2</option>
+<option value=\"3\">3</option>
+<option value=\"4\">4</option>
+<option value=\"5\">5</option>
+<option value=\"6\">6</option>
+<option value=\"7\">7</option>
+<option value=\"8\">8</option>
+<option value=\"9\" selected=\"selected\">9</option>
+<option value=\"10\">10</option>
+<option value=\"11\">11</option>
+<option value=\"12\">12</option>
+</select><select name=\"name_d\" id=\"name_d\" class=\"d\"><option value=\"1\">1</option>
+<option value=\"2\">2</option>
+<option value=\"3\">3</option>
+<option value=\"4\">4</option>
+<option value=\"5\">5</option>
+<option value=\"6\">6</option>
+<option value=\"7\">7</option>
+<option value=\"8\">8</option>
+<option value=\"9\">9</option>
+<option value=\"10\">10</option>
+<option value=\"11\">11</option>
+<option value=\"12\">12</option>
+<option value=\"13\">13</option>
+<option value=\"14\">14</option>
+<option value=\"15\">15</option>
+<option value=\"16\">16</option>
+<option value=\"17\">17</option>
+<option value=\"18\">18</option>
+<option value=\"19\">19</option>
+<option value=\"20\">20</option>
+<option value=\"21\">21</option>
+<option value=\"22\">22</option>
+<option value=\"23\">23</option>
+<option value=\"24\">24</option>
+<option value=\"25\">25</option>
+<option value=\"26\">26</option>
+<option value=\"27\">27</option>
+<option value=\"28\" selected=\"selected\">28</option>
+<option value=\"29\">29</option>
+<option value=\"30\">30</option>
+<option value=\"31\">31</option>
+</select></span>")))
+
 (defun list= (x y)
   (and (null (set-difference x y :test #'equal))
        (null (set-difference y x :test #'equal))))
